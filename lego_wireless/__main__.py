375e440362b905ff46d1db4b7e588ac05d1eb290
Alexander Dutton <adutton@oni.bio>
2019-07-21 13:36:55 +0100

    Add missing files

---
+++ lego_wireless/__main__.py
@@ -0,0 +1,41 @@
+import atexit
+import logging
+import threading
+
+from lego_wireless.hub_io import TrainMotor, LEDLight
+from .manager import HubManager
+from . import signals
+
+hubs_seen = set()
+
+def hub_discovered(sender, hub):
+    if hub not in hubs_seen:
+        hubs_seen.add(hub)
+        print("Connecting Hub IO, %s", hub.mac_address)
+        signals.hub_io_connected.connect(hub_io_connected, sender=hub)
+        hub.connect()
+
+
+def hub_io_connected(sender, hub_io):
+    print("Let's go!")
+    if isinstance(hub_io, TrainMotor):
+        hub_io.set_speed(100)
+    if isinstance(hub_io, LEDLight):
+        hub_io.set_brightness(100)
+
+
+def main():
+    hub_manager = HubManager('hci0')
+    atexit.register(hub_manager.stop)
+
+    signals.hub_discovered.connect(hub_discovered, sender=hub_manager)
+
+    hub_manager_thread = threading.Thread(target=hub_manager.run)
+    hub_manager_thread.start()
+    hub_manager.start_discovery()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
+
---
+++ lego_wireless/signals.py
@@ -0,0 +1,11 @@
+from blinker import signal
+
+hub_discovered = signal('hub-discovered')
+
+hub_connected = signal('hub-connected')
+hub_disconnected = signal('hub-disconnected')
+
+hub_io_connected = signal('hub-io-connected')
+hub_io_disconnected = signal('hub-io-disconnected')
+
+hub_battery_level = signal('hub-battery-level')

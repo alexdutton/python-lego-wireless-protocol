ab50e81ce29c0bc03acbc45ba7bf12a540818d93
Alexander Dutton <adutton@oni.bio>
2019-07-21 11:44:26 +0100

    Add README and setup.py

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lego Wireless Protocol for Python
+
+A spare-time implementation of the Lego Wireless Protocol for Python, to support a pet project.
+
+See `lego_wireless/__main__.py` for a usage example.
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='lego-wireless',
+    version='0.1',
+    description='Control Lego Powered Up devices over Bluetooth',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author='Alexander Dutton',
+    author_email='code@alexdutton.co.uk',
+    url="https://github.com/alexsdutton/python-lego-wireless-protocol",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "gatt",
+        "blinker",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

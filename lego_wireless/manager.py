fcfc3989a9562799f959cd980eccb8f657fabcd3
Alexander Dutton <adutton@oni.bio>
2019-07-21 11:47:01 +0100

    Better service discovery
    With the dbus backend, the filter is OR'd with other discovery requests from
    other applications, so we need to explicitly check any returned devices are the
    type we expected.

--- lego_wireless/manager.py
+++ lego_wireless/manager.py
@@ -1,29 +1,27 @@
 import gatt
 
+from .constants import SERVICE_UUID
+from .hub import Hub
+from . import signals
+
 
 class HubManager(gatt.DeviceManager):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.seen_devices = set()
-        self.trains = []
+        self.hubs = set()
 
     def make_device(self, mac_address):
         return Hub(mac_address=mac_address, manager=self)
 
     def device_discovered(self, device: gatt.Device):
-        if device.mac_address in self.seen_devices:
-            return
-        self.seen_devices.add(device.mac_address)
-        #        device.connect()
-        print(device)
-        print(device.services_resolved())
-        # print(device.services)
+        super().device_discovered(device)
+        if SERVICE_UUID in map(str, device._properties.Get('org.bluez.Device1', 'UUIDs')):
+            signals.hub_discovered.send(self, hub=device)
 
-    def hub_discovered(self, train):
-        self.trains.append(train)
-        print("Train discovered", train)
-        train.connect()
+    def start_discovery(self):
+        return super().start_discovery([SERVICE_UUID])
 
-    def run(self):
-        self.start_discovery()
-        super().run()
+    def stop(self):
+        for hub in self.hubs:
+            hub.disconnect()
+        super().stop()
